d5f1e7071faae228227536509e67b18a66004c45
Alexgl71 <avalente.me@icloud.com>
2018-07-11 21:13:14 +0200

    first

---
+++ Example/DropBoxFrmwk/DropBoxFrmwk.h
@@ -0,0 +1,19 @@
+//
+//  DropBoxFrmwk.h
+//  DropBoxFrmwk
+//
+//  Created by Alessandro Valente on 11/07/2018.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DropBoxFrmwk.
+FOUNDATION_EXPORT double DropBoxFrmwkVersionNumber;
+
+//! Project version string for DropBoxFrmwk.
+FOUNDATION_EXPORT const unsigned char DropBoxFrmwkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DropBoxFrmwk/PublicHeader.h>
+
+
---
+++ Example/DropBoxFrmwk/Helper.swift
@@ -0,0 +1,21 @@
+//
+//  Helper.swift
+//  DropBoxFrmwk
+//
+//  Created by Alessandro Valente on 11/07/2018.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import Foundation
+
+
+public class helper {
+    
+    public init() {
+        
+    }
+    
+    public func test() {
+        
+    }
+}
